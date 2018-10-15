9d5d53feec869347c89eec2448b7d26653ee8d33
John Poulson <Jdpoulson@gmail.com>
2018-10-15 11:24:43 -0700

    added index3 folder

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
